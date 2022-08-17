4332938557ab010e222abfc8b0376d8b0b05230a
Arina <arina_ivanchyk@mail.ru>
2022-08-17 11:27:46 +0300

    add ignore and file

---
+++ Tasks_HW/Program.cs
@@ -0,0 +1,52 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 ->
+
+//  Console.Write("input number A ");
+//  int numA = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("input number B ");
+//  int numB = Convert.ToInt32(Console.ReadLine());
+//  int num = 1;
+//  for (int i = 0; i < numB; i++)
+//  {
+//     num*= numA;
+//  }
+//  Console.Write(num);
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+//   Console.Write("input number ");
+//   int num = Convert.ToInt32(Console.ReadLine());
+//   Console.WriteLine($"product digits = {ProdNum(num)}");
+//   int ProdNum (int number)
+//   {
+//      int prod = 1;
+//      while (number >0)
+//      {
+//          prod*= number%10;
+//          number/=10;
+//      }
+//      return (prod);
+//  }
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+// int [] array = new int [8];
+// Random num = new Random ();
+// for (int i = 0; i < 8; i++)
+// {
+//     array[i] = num.Next(0,100);
+//     Console.Write(array[i] + " ");
+
+// }
+
